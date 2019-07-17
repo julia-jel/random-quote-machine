99ee7cfa535afee791ca9658d231aacabbd8a9c2
julia-jel <44364383+julia-jel@users.noreply.github.com>
2019-07-17 12:57:15 +0300

    Create Footer.jsx

---
+++ app/components/Footer.jsx
@@ -0,0 +1,18 @@
+const React = require('react');
+
+class Footer extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  
+  render() {
+    return (
+      <div id="footer">
+        <p>Designed and coded by</p> 
+        <a id="link" target="_blank" href="https://www.freecodecamp.org/julia-el">Julia El</a>
+      </div>
+    )
+  }
+};
+
+module.exports = Footer;
