52a6b0aba124ca6ff99310ed8d4ea4a5fa1ce276
julia-jel <44364383+julia-jel@users.noreply.github.com>
2019-07-17 13:16:10 +0300

    Create myApp.jsx

---
+++ app/myApp.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/* Import Components */
+import QuoteBox from './components/QuoteBox';
+
+ReactDOM.render(<QuoteBox />, document.getElementById('root'));
