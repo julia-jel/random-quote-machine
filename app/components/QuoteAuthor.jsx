08090b7b8307f9ebb58ae13498f31ec5801321f3
julia-jel <44364383+julia-jel@users.noreply.github.com>
2019-07-17 12:59:48 +0300

    Create QuoteAuthor.jsx

---
+++ app/components/QuoteAuthor.jsx
@@ -0,0 +1,19 @@
+const React = require('react');
+
+/* Rendering a quote's author passed as props */
+
+class QuoteAuthor extends React.Component {
+  constructor(props) {
+    super(props);
+  };
+  
+  render() {
+    return (
+      <div id={this.props.id}>
+        {"- " + this.props.author}
+      </div>
+    )
+  };
+}
+
+module.exports = QuoteAuthor;
