a267c68080dfe2b3088388c9c1ae7e315505efdd
julia-jel <44364383+julia-jel@users.noreply.github.com>
2019-07-17 12:58:54 +0300

    Create NewQuoteButton.jsx

---
+++ app/components/NewQuoteButton.jsx
@@ -0,0 +1,17 @@
+const React = require('react');
+
+/* Creating a button for changing a quote, an image and a background color */
+
+class NewQuoteButton extends React.Component {
+  constructor(props) {
+    super(props);
+  };
+    
+    render() {
+      return (
+        <button id={this.props.id} onClick={this.props.handleClick} class="btn btn-primary" title="Get a new quote!">New Quote</button>
+      );
+    }
+};
+
+module.exports = NewQuoteButton;
